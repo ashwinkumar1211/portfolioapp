369e58eb8fb0dc32e93abb836a1bc6834a5e8514
Ashwincodez <ashwinkumar.m1211@gmail.com>
2022-02-12 18:27:23 +0530

    Add

---
+++ build/precache-manifest.4fbad21a2928107cdd2c43fd704a2e48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce381cca078a1216ee702016dcfebd03",
+    "url": "/Digital-Portfolio/index.html"
+  },
+  {
+    "revision": "6417576d16b09939baa6",
+    "url": "/Digital-Portfolio/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "1c92296fb31b728ff1b1",
+    "url": "/Digital-Portfolio/static/css/main.1d89b8f9.chunk.css"
+  },
+  {
+    "revision": "6417576d16b09939baa6",
+    "url": "/Digital-Portfolio/static/js/2.4c9e5701.chunk.js"
+  },
+  {
+    "revision": "ace9d686b527aac01ed6b880850d43a3",
+    "url": "/Digital-Portfolio/static/js/2.4c9e5701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c92296fb31b728ff1b1",
+    "url": "/Digital-Portfolio/static/js/main.2b19d1ba.chunk.js"
+  },
+  {
+    "revision": "e108e20b14e3bbc81cd9",
+    "url": "/Digital-Portfolio/static/js/runtime-main.3a6a82d7.js"
+  }
+]);
